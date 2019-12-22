06ef3d3c3b5a3839171a45b2964129a02bffdd7a
Dumery <laurie@macbook-pro-de-dumery.home>
2019-12-22 22:36:58 +0100

    Improve loading method and change the model

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const Scene = {
         scene: null,
         renderer: null,
         camera: null,
-        objects: []
+        draggableObjects: [],
     },
 
     animate: () => {
@@ -30,6 +30,36 @@ const Scene = {
         vars.camera.aspect = window.innerWidth / window.innerHeight; 
         vars.camera.updateProjectionMatrix(); // MAJ de l'aspect de la camera
         vars.renderer.setSize(window.innerWidth, window.innerHeight); // Change la taille du render
+    },
+
+    loadGLTF: (file, draggable) => {
+        let loader = new GLTFLoader();
+
+        loader.load(
+            './models/' + file,
+            function(gltf) {
+                Scene.vars.scene.add(gltf.scene);
+
+                var childrenModel = gltf.scene.children[0].children[0].children[0].children[0].children; // pour dissocier chaque enfants du model
+                var numDraggableObjects = [14, 17, 18, 25, 35, 39, 41, 42, 43, 46, 47, 66, 87, 88, 89]; // numéros  des enfants du model qui seront draggables
+                var numRemoveObjects = [31, 97, 102, 103]; // numéros des enfants du model qui ne seront pas visibles
+                var noDraggableObjects = new THREE.Group();
+
+                for(var i = 0; i < childrenModel.length; i++) {
+                    if (numDraggableObjects.includes(i)){
+                        Scene.vars.draggableObjects.push(childrenModel[i]); // ajout de la possibilité de bouger les petits objets
+                        childrenModel[i].position.x = -500;
+                    }
+                    if (numRemoveObjects.includes(i)) {
+                        childrenModel[i].position.x = -5000;
+                        childrenModel[i].position.y = -5000;
+                    }
+                }
+
+                // console.log(vars.);
+                // callback();
+            }
+        );
     },
 
     init: () => {
@@ -62,24 +92,13 @@ const Scene = {
         light.position.set(0,700, 0);
         vars.scene.add(light);
 
-        // Ajout des modèles 3D
-        let loader = new GLTFLoader();
-        loader.load(
-            './models/scene.gltf',
-            function(gltf) {
-                vars.scene.add(gltf.scene);
-                vars.objects.push(gltf.scene)
-            }
-        );
+        let grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
+        grid.material.opacity = 0.2;
+        grid.material.transparent = true;
+        vars.scene.add(grid);
 
-        let loader2 = new GLTFLoader();
-        loader2.load(
-            './models/apple/scene.gltf',
-            function(gltf) {
-                vars.scene.add(gltf.scene);
-                vars.objects.push(gltf.scene)
-            }
-        );
+        // Ajout du modèle 3D
+        Scene.loadGLTF('scene.gltf', true);
 
         // Ajout du controleur sur la camera
         vars.controls = new OrbitControls(vars.camera, vars.renderer.domElement);
@@ -87,25 +106,17 @@ const Scene = {
 
         // Pour limiter les mouvements de la caméra
         vars.controls.minDistance = 300;
-        vars.controls.maxDistance = 600;
+        vars.controls.maxDistance = 1200;
         vars.controls.minPolarAngle = Math.PI / 3;
         vars.controls.maxPolarAngle = Math.PI / 3;
-        vars.controls.minAzimuthAngle = Math.PI / 4;
-        vars.controls.maxAzimuthAngle = Math.PI / 4;
+        vars.controls.minAzimuthAngle = -Math.PI/5;
+        vars.controls.maxAzimuthAngle = -Math.PI/5;
 
         vars.controls.update();
 
         // Ajout du controler draggable
 
-        var controls2 = new DragControls( vars.objects, vars.camera, vars.renderer.domElement );
-        
-        // add event listener to highlight dragged objects
-        controls2.addEventListener( 'dragstart', function ( event ) {
-            event.object.material.emissive.set( 0xaaaaaa );
-        } );
-        controls2.addEventListener( 'dragend', function ( event ) {
-            event.object.material.emissive.set( 0x000000 );
-        } );
+        var controls2 = new DragControls( vars.draggableObjects, vars.camera, vars.renderer.domElement );
 
         // Pour gérer le redimentionnment de la fenêtre
         window.addEventListener('resize', Scene.onWindowResize, false);
