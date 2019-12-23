eaa355abe3652561c55d0a43f1fc43a96602cb8c
Dumery <laurie@macbook-pro-de-dumery.home>
2019-12-23 03:21:22 +0100

    Improve design and interactivity

--- js/scene.js
+++ js/scene.js
@@ -1,5 +1,4 @@
 import * as THREE from '../vendor/three.js-master/build/three.module.js';
-import Stats from '../vendor/three.js-master/examples/jsm/libs/stats.module.js';
 import { OrbitControls } from '../vendor/three.js-master/examples/jsm/controls/OrbitControls.js';
 import { DragControls } from '../vendor/three.js-master/examples/jsm/controls/DragControls.js';
 import { GLTFLoader } from '../vendor/three.js-master/examples/jsm/loaders/GLTFLoader.js'; 
@@ -23,35 +22,39 @@ const Scene = {
 
     render: () => {
         Scene.vars.renderer.render(Scene.vars.scene, Scene.vars.camera);
-        Scene.vars.stats.update();
     },
 
     onClickOnOrbitControl : () => {
-
         // Ajout du controleur sur la camera
-
         if(Scene.vars.controls != null) {
             Scene.vars.controls.enabled = false;
         }
 
         Scene.vars.controls = new OrbitControls(Scene.vars.camera, Scene.vars.renderer.domElement);
-        Scene.vars.controls.target.set(0, 0, 0);
+        Scene.vars.controls.target.set(0, 200, 0);
+
+        document.getElementById("dragControl").classList.remove("selected");
+        document.getElementById("orbitControl").classList.add("selected");
 
         // Pour limiter les mouvements de la caméra
         Scene.vars.controls.minDistance = 300;
-        Scene.vars.controls.maxDistance = 1300;
+        Scene.vars.controls.maxDistance = 1400;
         Scene.vars.controls.minPolarAngle = Math.PI / 4;
         Scene.vars.controls.maxPolarAngle = Math.PI / 2;
         Scene.vars.controls.minAzimuthAngle = -Math.PI/2;
-        Scene.vars.controls.maxAzimuthAngle = Math.PI/8;
+        Scene.vars.controls.maxAzimuthAngle = 0;
         Scene.vars.controls.update();
+
+        console.log(Scene.vars.controls.getAzimuthalAngle());
+        console.log(Scene.vars.controls.getPolarAngle());
     },
 
     onClickOnDragControl : () => {
             // Ajout du controleur draggable
             Scene.vars.controls.enabled = false;
             Scene.vars.controls = new DragControls( Scene.vars.draggableObjects, Scene.vars.camera, Scene.vars.renderer.domElement );
-            console.log(Scene.vars.controls);
+            document.getElementById("orbitControl").classList.remove("selected");
+            document.getElementById("dragControl").classList.add("selected");
     },
 
     onWindowResize: () => {
@@ -61,32 +64,31 @@ const Scene = {
         vars.renderer.setSize(window.innerWidth, window.innerHeight); // Change la taille du render
     },
 
-    loadGLTF: (file, draggable) => {
+    loadGLTF: (file) => {
         let loader = new GLTFLoader();
 
-        loader.load(
-            './models/' + file,
-            function(gltf) {
+        // Chargement du modèle
+        loader.load('./models/' + file, function(gltf) {
                 Scene.vars.scene.add(gltf.scene);
 
-                var childrenModel = gltf.scene.children[0].children[0].children[0].children[0].children; // pour dissocier chaque enfants du model
-                var numDraggableObjects = [14, 17, 18, 25, 35, 39, 41, 42, 43, 46, 47, 66, 87, 88, 89]; // numéros  des enfants du model qui seront draggables
-                var numRemoveObjects = [31, 97, 102, 103]; // numéros des enfants du model qui ne seront pas visibles
-                var noDraggableObjects = new THREE.Group();
+                var childrenModel = gltf.scene.children[0].children[0].children[0].children[0].children; // Pour dissocier chaque enfants du model
+                var numDraggableObjects = [14, 17, 18, 35, 39, 41, 42, 43, 46, 47, 66, 87, 88, 89]; // Numéros  des enfants du model qui seront draggables
+                var numRemoveObjects = [31, 97, 102, 103]; // Numéros des enfants du model qui ne seront pas visibles
 
                 for(var i = 0; i < childrenModel.length; i++) {
+                    
+                    // Ajout de la possibilité de bouger les petits objets
                     if (numDraggableObjects.includes(i)){
-                        Scene.vars.draggableObjects.push(childrenModel[i]); // ajout de la possibilité de bouger les petits objets
+                        Scene.vars.draggableObjects.push(childrenModel[i]); 
                         childrenModel[i].position.x = -500;
                     }
+
+                    // Cache les petits objets moches
                     if (numRemoveObjects.includes(i)) {
                         childrenModel[i].position.x = -5000;
                         childrenModel[i].position.y = -5000;
                     }
                 }
-
-                // console.log(vars.);
-                // callback();
             }
         );
     },
@@ -101,7 +103,7 @@ const Scene = {
 
         // Création de la scène
         vars.scene = new THREE.Scene();
-        vars.scene.background = new THREE.Color(0x6ab2c7);
+        vars.scene.background = new THREE.Color(0xf4bebe);
 
         // Création renderer
         vars.renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -112,19 +114,14 @@ const Scene = {
         vars.container.appendChild(vars.renderer.domElement);
 
         // Ajout de la camera avec plusieurs paramètres
-        vars.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 2, 2000);
-        vars.camera.position.set(-200, 1000, 500);
+        vars.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 2, 2000);
+        vars.camera.position.set(-1000, 0, 1000);
 
         // Création de la lumière globale
         const lightIntensityHemisphere = 1;
-        let light = new THREE.HemisphereLight(0xFFFFFF, 0x444444,lightIntensityHemisphere);
+        let light = new THREE.HemisphereLight(0xFFFFFF, 0x888888,lightIntensityHemisphere);
         light.position.set(0,700, 0);
         vars.scene.add(light);
-
-        let grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
-        grid.material.opacity = 0.2;
-        grid.material.transparent = true;
-        vars.scene.add(grid);
 
         // Ajout du modèle 3D
         Scene.loadGLTF('scene.gltf', true);
@@ -136,9 +133,6 @@ const Scene = {
 
         // Gestion du redimentionnment de la fenêtre
         window.addEventListener('resize', Scene.onWindowResize, false);
-
-        vars.stats = new Stats;
-        vars.container.appendChild(vars.stats.dom);
 
         Scene.animate();
     }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function play() {
-    document.getElementById('home').parentNode.removeChild(document.getElementById('home')); // Pour retirer le loading une fois chargé
+    document.getElementById('home').style.display = "none"; // Pour retirer le loading une fois chargé
+}
+
+function credits() {
+    document.getElementById("credits").style.display = "block";
+    console.log("ok");
+}
+
+function home() {
+    document.getElementById('home').style.display = "block";;
+    document.getElementById("credits").style.display = "none";
 }
